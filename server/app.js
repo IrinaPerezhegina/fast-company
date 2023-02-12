285d83baebd92bc84bc8b9b470573e74745ea754
IrinaPerezhegina <perezeginaiv@mail.ru>
2023-02-12 17:25:08 +0500

    added server

---
+++ server/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+const cors = require("cors");
+const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/api", routes);
+
+mongoose.set("strictQuery", true);
+const PORT = config.get("port") ?? 8080;
+// if (process.env.NODE_ENV === "production") {
+//   console.log("production");
+// } else {
+//   console.log("development");
+// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+  const indexPath = path.join(__dirname, "client", "index.html");
+  app.get("*", (req, res) => {
+    res.send(indexPath);
+  });
+}
+
+async function start() {
+  try {
+    mongoose.connection.once("open", () => {
+      initDatabase();
+    });
+    await mongoose.connect(config.get("mongoUri"));
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server has been started on port ${PORT}...`));
+    });
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+start();
